38c32217e3e67d1ed4a05d391448c3044da3b326
DarkZ <146022304+RISHABH12005@users.noreply.github.com>
2025-05-08 00:49:15 +0530

    Update LAB7-Q2.py

--- Python/LAB7-Q2.py
+++ Python/LAB7-Q2.py
@@ -1,41 +1,41 @@
-class Edge:
-    def __init__(self, u, v, weight):
+class E:
+    def __init__(self, u, v, w):
         self.u = u
         self.v = v
-        self.weight = weight
-def find(parent, i):
-    if parent[i] != i:
-        parent[i] = find(parent, parent[i])
-    return parent[i]
-def union(parent, rank, x, y):
-    root_x = find(parent, x)
-    root_y = find(parent, y)
-    if rank[root_x] < rank[root_y]:
-        parent[root_x] = root_y
-    elif rank[root_x] > rank[root_y]:
-        parent[root_y] = root_x
+        self.w = w
+def f(p, i):
+    if p[i] != i:
+        p[i] = f(p, p[i])
+    return p[i]
+def u(p, r, x, y):
+    rtx = f(p, x)
+    rty = f(p, y)
+    if r[rtx] < r[rty]:
+        p[rtx] = rty
+    elif r[rtx] > r[rty]:
+        p[rty] = rtx
     else:
-        parent[root_y] = root_x
-        rank[root_x] += 1
-def kruskal(n, edges):
-    edges.sort(key=lambda e: e.weight)
-    parent = [i for i in range(n)]
-    rank = [0] * n
-    mst = []
-    for edge in edges:
-        if find(parent, edge.u) != find(parent, edge.v):
-            mst.append(edge)
-            union(parent, rank, edge.u, edge.v)
+        p[rty] = rtx
+        r[rtx] += 1
+def k(n, e):
+    e.sort(key=lambda e: e.w)
+    p = [i for i in range(n)]
+    r = [0] * n
+    m = []
+    for e in es:
+        if f(p, e.u) != f(p, e.v):
+            m.append(e)
+            u(p, r, e.u, e.v)
     print("Edge \tWeight")
-    for edge in mst:
-        print(f"{edge.u} - {edge.v} \t{edge.weight}")
-edges = [
-    Edge(0, 1, 2),
-    Edge(0, 3, 6),
-    Edge(1, 2, 3),
-    Edge(1, 3, 8),
-    Edge(1, 4, 5),
-    Edge(2, 4, 7),
-    Edge(3, 4, 9)
+    for e in m:
+        print(f"{e.u} - {e.v} \t{e.w}")
+es = [
+    E(0, 1, 2),
+    E(0, 3, 6),
+    E(1, 2, 3),
+    E(1, 3, 8),
+    E(1, 4, 5),
+    E(2, 4, 7),
+    E(3, 4, 9)
 ]
-kruskal(5, edges)
+k(5, es)
