b096b9ca4dbfb5445f94d87411725f14569e83e9
DarkZ <146022304+RISHABH12005@users.noreply.github.com>
2025-05-08 00:39:45 +0530

    Update LAB7-Q1.py

--- Python/LAB7-Q1.py
+++ Python/LAB7-Q1.py
@@ -1,31 +1,31 @@
 import sys
-def prim(graph):
-    n = len(graph)
-    selected = [False] * n
-    key = [sys.maxsize] * n
-    parent = [None] * n
-    key[0] = 0
-    parent[0] = -1
+def pr(g):
+    n = len(g)
+    s = [False] * n
+    k = [sys.maxsize] * n
+    p = [None] * n
+    k[0] = 0
+    p[0] = -1
     for _ in range(n):
-        min_key = sys.maxsize
+        mk = sys.maxsize
         u = -1
         for v in range(n):
-            if not selected[v] and key[v] < min_key:
-                min_key = key[v]
+            if not s[v] and k[v] < mk:
+                mk = k[v]
                 u = v
-        selected[u] = True
+        s[u] = True
         for v in range(n):
-            if graph[u][v] and not selected[v] and graph[u][v] < key[v]:
-                key[v] = graph[u][v]
-                parent[v] = u
+            if g[u][v] and not s[v] and g[u][v] < k[v]:
+                k[v] = g[u][v]
+                p[v] = u
     print("Edge \tWeight")
     for i in range(1, n):
-        print(f"{parent[i]} - {i} \t{graph[i][parent[i]]}")
-graph = [
+        print(f"{p[i]} - {i} \t{g[i][p[i]]}")
+g = [
     [0, 2, 0, 6, 0],
     [2, 0, 3, 8, 5],
     [0, 3, 0, 0, 7],
     [6, 8, 0, 0, 9],
     [0, 5, 7, 9, 0]
 ]
-prim(graph)
+pr(g)
