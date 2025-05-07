310fa4924d44aafdda8cb5d4d0e25df7b4b71500
DarkZ <146022304+RISHABH12005@users.noreply.github.com>
2025-05-08 01:03:06 +0530

    Update LAB5-Q1.py

--- Python/LAB5-Q1.py
+++ Python/LAB5-Q1.py
@@ -1,4 +1,4 @@
-def partition(a, l, h):
+def pt(a, l, h):
     p = a[h]
     i = l - 1
     for j in range(l, h):
@@ -7,11 +7,11 @@ def partition(a, l, h):
             a[i], a[j] = a[j], a[i]
     a[i + 1], a[h] = a[h], a[i + 1]
     return i + 1
-def quick(a, l, h):
+def q(a, l, h):
     if l < h:
-        pi = partition(a, l, h)
-        quick(a, l, pi - 1)
-        quick(a, pi + 1, h)
+        pi = pt(a, l, h)
+        q(a, l, pi - 1)
+        q(a, pi + 1, h)
 a= [21, 51, 31, 1, 22, 32, 2, 33]
-quick(a, 0, len(a) - 1)
+q(a, 0, len(a) - 1)
 print("Quick Sort:", a)
